8ab0c0a38d62ceb17c11108590761dcbc2a6714f
mustafa695 <amustufa37@gmail.com>
2024-10-01 16:41:56 +0500

    added logged in user data in response

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -22,12 +22,13 @@ router.post("/login", async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
-    maxAge: 24 * 60 * 60 * 1000, 
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
   });
   return res.json({
     message: "Login successful",
     success: true,
+    data: user,
   });
 });
 
